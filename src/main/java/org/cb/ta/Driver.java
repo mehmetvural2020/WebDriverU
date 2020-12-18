0e1baadf17678fba95e4617ee18267c663f6c736
mehmetvural2020 <mehmetvurall@gmail.com>
2020-12-18 15:23:02 +0700

    Driver file created

--- src/main/java/org/cb/ta/Driver.java
+++ src/main/java/org/cb/ta/Driver.java
@@ -1,2 +1,15 @@
-package org.cb.ta;public class Driver {
+package org.cb.ta;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Driver {
+    private static WebDriver driver;
+    public static WebDriver getDriver(){
+        System.setProperty("webdriver.chromedriver", "webdriver/chromedriver");
+        driver = new ChromeDriver();
+        return driver;
+
+
+    }
 }
